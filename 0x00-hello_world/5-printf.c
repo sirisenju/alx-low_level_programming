7a5fa7dd796e0e50827b1d22629f9bb33b7cbbbe
sirisenju <amrohore4real@gmail.com>
2022-10-13 19:48:26 -0700

    printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the content
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
