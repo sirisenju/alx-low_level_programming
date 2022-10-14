693c486eb569e522b3546f15e4cbf0547323908d
sirisenju <amrohore4real@gmail.com>
2022-10-13 18:26:23 -0700

    print out ib c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main()
-{	
-	printf("Programming is like building a multilingual puzzle\n");
+/**
+ *  *main- Serves as the entry point for the program
+ *   *
+ *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *     */
+int main(void){
+	printf("Programming is like building a multilingual game\n");
 	return (0);
 }
