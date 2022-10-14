b0ffb69ea02408436b2ef6c9c7455f0350a6f394
sirisenju <amrohore4real@gmail.com>
2022-10-13 19:54:43 -0700

    printf bug fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Prints out the content
  *
- * Return: Always 0(success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammer, but the outcome is a piece of art");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
