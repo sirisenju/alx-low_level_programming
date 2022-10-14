79f7e1bb9a79b068730b8dd39f23ec44c5f4d666
sirisenju <amrohore4real@gmail.com>
2022-10-13 19:03:50 -0700

    bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/* 
+/*
  * * main Serves as the entry point for the program
  ** Return By default returns zero if no erros otherwise returns non-zero value
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual game");
+	char text[] = "Programming is like building a multilingual game";
+
+	puts(text);
+
 	return (0);
 }
