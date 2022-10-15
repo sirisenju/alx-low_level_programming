39ea15b8e16872281045d690759413ea41a3933c
sirisenju <amrohore4real@gmail.com>
2022-10-14 17:59:03 -0700

    the zero game

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - executes the code
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	lastDigit = n % 10;
+
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+}
