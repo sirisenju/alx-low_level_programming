5c21a5ed7094cb1d714ca7f80fedceee7ace71d7
sirisenju <amrohore4real@gmail.com>
2022-10-14 17:40:51 -0700

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int n;
 
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -23,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("\d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
