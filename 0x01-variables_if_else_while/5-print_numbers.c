adbf5510c9c9ef1f264463cadb25046819525d5f
sirisenju <amrohore4real@gmail.com>
2022-10-16 15:39:18 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - executes body of code
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		puts(i);
+	}
+	return (0);
+}
