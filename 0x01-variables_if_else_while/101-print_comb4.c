f6ef29d4ef91955b4f175b0d9d106c6124285cf0
sirisenju <amrohore4real@gmail.com>
2022-10-16 18:08:56 -0700

    3 digits combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - body of the code
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			k = 48;
+			while (k < 48)
+			{
+				if (j != i && j != k && i != k && j < i && i < k)
+				{
+					putchar(j);
+					putchar(i);
+					putchar(k);
+
+					if (i == 56 && j == 55 && k == 57)
+					{
+						break;
+					}
+				}
+
+				k++
+
+			}
+
+			i++
+
+		}
+
+		j++
+	}
+
+}
