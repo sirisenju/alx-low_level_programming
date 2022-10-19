0c3a5fb751576bf1955977aa56087639e8f0525a
sirisenju <amrohore4real@gmail.com>
2022-10-19 15:37:35 -0700

    print all alphabets prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * Return: Always 0 (Success)
  *
  */
-void print_alphabet(void)
+int main(void)
 {
 	char alpha = 'a';
 
 	while (alpha <= 'z')
 	{
-		_putchar(alpha);
+		print_alphabet(alpha);
 
 		alpha++;
 	}
