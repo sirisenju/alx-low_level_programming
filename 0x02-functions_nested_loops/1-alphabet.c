379e63e3ce2534da6a952eb6f053b53f12009ba4
sirisenju <amrohore4real@gmail.com>
2022-10-19 15:33:38 -0700

    print alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Return: Always 0 (Success)
  *
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alpha = 'a';
 
@@ -16,4 +16,6 @@ void print_alphabet(void);
 	}
 
 	_putchar('\n');
+
+	return (0);
 }
