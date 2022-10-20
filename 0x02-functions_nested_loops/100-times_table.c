6ee6edc40475bb5974e5d741ec97b08844296856
sirisenju <amrohore4real@gmail.com>
2022-10-19 19:46:07 -0700

    100x

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,33 +11,36 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= n; mult++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			ans = num * mult;
-
-			if (ans <= 99)
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
 			{
+				_putchar(',');
 				_putchar(' ');
-			}
-			if (ans <= 9)
-			{
-				_putchar(' ');
-			}
-			if (ans >= 100)
-			{
-				_putchar((ans / 100) + '0');
-				_putchar(((ans / 10)) % 10 + '0');
-			}
-			else if (ans <= 99 && ans >= 10)
-			{
-				_putchar((ans / 10) + '0');
-			}
 
+				ans = num * mult;
+
+				if (ans <= 99)
+				{
+					_putchar(' ');
+				}
+				if (ans <= 9)
+				{
+					_putchar(' ');
+				}
+				if (ans >= 100)
+				{
+					_putchar((ans / 100) + '0');
+					_putchar(((ans / 10)) % 10 + '0');
+				}
+				else if (ans <= 99 && ans >= 10)
+				{
+					_putchar((ans / 10) + '0');
+				}
+
+				_putchar((ans % 10) + '0');
+			}
 			_putchar((ans % 10) + '0');
 		}
 		_putchar('\n');
