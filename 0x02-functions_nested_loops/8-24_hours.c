e1df84cc6c44b0c10752b61395d7e19802d746f3
sirisenju <amrohore4real@gmail.com>
2022-10-19 18:04:22 -0700

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int hour, min;
 
-	for (hour = 0; hour >= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min = 0; min >= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
