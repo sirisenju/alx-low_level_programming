e24c90539147ce2cf469f82a7f7c9868397995d2
sirisenju <amrohore4real@gmail.com>
2022-10-20 12:08:40 -0700

    initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - executes the body of the code
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
