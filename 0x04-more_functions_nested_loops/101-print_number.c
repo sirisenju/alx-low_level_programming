2110c94313952f9a5e0a46d8cd4c9d434f7aa569
sirisenju <amrohore4real@gmail.com>
2022-10-20 18:56:40 -0700

    final commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ * @n: integer type
+ * Return: void
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
