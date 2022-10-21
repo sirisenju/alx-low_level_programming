08750ebb391411a96f642ab244b424187ab946e9
sirisenju <amrohore4real@gmail.com>
2022-10-20 17:41:58 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - executes the body of the code
+ * Return: 0 successful
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
