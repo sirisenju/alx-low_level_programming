6581365544324ed50d3521075276695f0d1b55d5
sirisenju <amrohore4real@gmail.com>
2022-10-20 17:15:12 -0700

    multiple of 2 int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - returns the multiple of 2 int
+ * @a: first int
+ * @b: second int
+ * Return: value success
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
