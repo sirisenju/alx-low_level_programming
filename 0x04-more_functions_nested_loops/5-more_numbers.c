eef75feaad39dc908bcc32f4d7b9f0659f58c677
sirisenju <amrohore4real@gmail.com>
2022-10-20 18:03:03 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 			{
-				_putchar((j / 10) + '0')
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
-			_putchar((j % 10) + '0')
 		}
 
 		_putchar('\n');
