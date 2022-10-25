7f1ceca3d7a8e541dcd21f0ae09415526ae79ed2
sirisenju <amrohore4real@gmail.com>
2022-10-24 17:30:11 -0700

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_rev _ prints the reverse of a string passed
+ * print_rev - prints the reverse of a string passed
  * @s: the passed in string
  * Return: void
+ *
  */
 
 void print_rev(char *s)
