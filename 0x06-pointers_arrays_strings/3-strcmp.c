fdc9c7ffd450ad176a0052548a5d248b75a87ddc
sirisenju <amrohore4real@gmail.com>
2022-10-27 16:19:04 -0700

    compares 2 string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - returns the comperison of 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: returns the compersion of 2 strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int num = 0;
+
+	while (*s1 == *s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 != s2)
+	{
+		num = *s1 - *s2;
+	}
+
+	return (num)
+}
