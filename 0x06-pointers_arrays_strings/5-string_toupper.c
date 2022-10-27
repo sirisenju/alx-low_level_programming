244e4773806146dc35750742a5e2b44ca2368b51
sirisenju <amrohore4real@gmail.com>
2022-10-27 16:51:32 -0700

    returns uppercase string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,17 @@
 
 char *string_toupper(char *string)
 {
+	int num = 0;
+
 	while (*(string + num) != '\0')
 	{
 		if ((*(string + num) >= 97) && (*(string + num) <= 122))
 		{
 			*(string + num) = *(string + num) - 32;
 		}
+
 		num++;
 	}
+
 	return (string);
 }
