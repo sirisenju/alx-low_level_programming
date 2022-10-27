09fbfceedbfb4b31764a6f379093e5a7e568612b
sirisenju <amrohore4real@gmail.com>
2022-10-26 17:37:57 -0700

    main.h fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /*
- *
- * * function prototype
- *
- * */
+ * function prototype
+ */
+
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
