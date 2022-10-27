7481ae7364324c00cac236acc641d54f8032f316
sirisenju <amrohore4real@gmail.com>
2022-10-26 17:34:12 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.c"
+
+/**
+ * _strcat - returns the concattination of 2 strings
+ * @dest: the second string
+ * @src: the input string
+ * Return: returns the result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int num1, num2;
+
+	num1 = 0;
+	while (dest[num1] != '\0')
+	{
+		num1++;
+	}
+
+	num2 = 0;
+	while (src[num2] != '\0')
+	{
+		dest[num1] = src[num2];
+		num1++;
+		num2++;
+	}
+	dest[num1] = '\0';
+
+	return (dest);
+}
