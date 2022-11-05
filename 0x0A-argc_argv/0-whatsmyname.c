47bb56c668d3d324dc194921898f3e358e8e02d3
sirisenju <amrohore4real@gmail.com>
2022-11-04 19:35:04 -0700

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,13 @@
  * main - main body of the code
  * @argc: contains the number of arguments
  * @argv: pointer of pointers containing strings
- * Return: 0 on success
- */
+ * Return: 0 on success */
 
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
 		printf("%s\n", argv[0]);
-		return (0);
 	}
+	return (0);
 }
