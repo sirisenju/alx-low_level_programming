828b1f4d2a77bb0887091d4de81e6ebbda91e582
sirisenju <amrohore4real@gmail.com>
2022-11-17 20:06:40 -0800

    prints string

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,8 +22,7 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			}
-			j++;
+			}, j++;
 		}
 		switch (format[i])
 		{
@@ -45,8 +44,8 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-		}
-		i++;
+		}, i++;
 	}
 	printf("\n"), va_end(valist);
 }
+
